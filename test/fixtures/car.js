330ebfa5840d4c1dfb3e88fb7f48a2a62251318e
Philip Walton <philip@philipwalton.com>
2014-03-29 14:13:37 -0700

    Fix jshint errors in the Car fixture.

--- test/fixtures/car.js
+++ test/fixtures/car.js
@@ -15,45 +15,46 @@ Car.prototype.drive = function(miles) {
 
 Car.prototype.getMileage = function() {
   return _(this).mileage;
-}
+};
 
 Car.prototype.getMilesSinceLastOilChange = function() {
   return _(this).mileage - _(this).mileageAtLastOilChange;
-}
+};
 
 Car.prototype.getMilesSinceLastTireRotation = function() {
   return _(this).mileage - _(this).mileageAtLastTireRotation;
-}
+};
 
 Car.prototype.changeOil = function() {
   if ( _(this).shouldChangeOil() ) {
     _(this).mileageAtLastOilChange = _(this).mileage;
   } else {
-    return("No oil change is needed at this time.")
+    return('No oil change is needed at this time.');
   }
-}
+};
 
 Car.prototype.rotateTires = function() {
   if ( _(this).shouldRotateTires() ) {
     _(this).mileageAtLastTireRotation = _(this).mileage;
   } else {
-    return("No tire rotation is needed at this time.")
+    return('No tire rotation is needed at this time.');
   }
-}
+};
 
-// Create the "private prototype".
+// Create the 'private prototype'.
 var privateMethods = Object.create(Car.prototype);
 
-// Add methods to the "private prototype".
+// Add methods to the 'private prototype'.
 privateMethods.shouldChangeOil = function() {
-  return this.getMilesSinceLastOilChange() > 5000 ? true : false
+  return this.getMilesSinceLastOilChange() > 5000 ? true : false;
 };
 
 privateMethods.shouldRotateTires = function() {
-  return this.getMilesSinceLastTireRotation() > 5000 ? true : false
+  return this.getMilesSinceLastTireRotation() > 5000 ? true : false;
 };
 
 // Create the key function with setting the private methods.
 var _ = require('../../').createKey(privateMethods);
 
 module.exports = Car;
+
